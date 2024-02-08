70cbe7af610d959585139c170c848472eb496f38
matheus mozart <mmsnborges@gmail.com>
2024-02-08 13:15:28 -0400

    first commit

---
+++ src/service/ReajuesteServoce.java
@@ -0,0 +1,27 @@
+package service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+
+import model.Funcionario;
+import validations.ValidacaoException;
+
+public class ReajuesteServoce {
+
+	public void reajusteSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
+
+		BigDecimal salarioAtual = funcionario.getSalario();
+		BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+
+		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
+			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+		}
+		
+		BigDecimal salarioReajustado = salarioAtual.add(aumento);
+		
+		funcionario.atulizarSalario(salarioReajustado);
+
+	}
+
+}
